3e9d1c31a735cc86223e4cfa7addc8d7415fa2c9
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2023-09-28 04:50:17 +0800

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -8,15 +8,13 @@ let handler = async(m, { conn, usedPrefix, command }) => {
     let message = `
 *We appreciate your support! 🙏*
     
-To help us keep improving, consider following us on Instagram. Every new follower brings us joy and motivation! 
-
-Our Instagram: @asli_guru_2k23
+donate ini bertujuan untuk melanjutkan bot ini agar ttp hidup
 
 Remember, even the smallest support can make a huge difference!
 
 Additionally, if you'd like to contribute directly, you can use the attached Google Pay QR code. Just scan it with your Google Pay app to send a donation. Your support means the world to us!`;
 
-    let img = 'https://telegra.ph/file/005c11a341e1f038786e5.jpg'; 
+    let img = 'https://drive.google.com/file/d/1-ahJ4OdWp8l35NaABlwcII3Dx6rMzlFQ/view?usp=drivesdk'; 
 
     console.log("Sending message and image...");
     await conn.sendFile(m.chat, img, 'donation.jpg', message, m, false, rpyp);
