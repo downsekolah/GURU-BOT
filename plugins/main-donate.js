3daf447bfbc080a991b5bcd2470301a5d82982d7
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2023-09-28 14:51:40 +0800

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -14,7 +14,7 @@ Remember, even the smallest support can make a huge difference!
 
 Additionally, if you'd like to contribute directly, you can use the attached Google Pay QR code. Just scan it with your Google Pay app to send a donation. Your support means the world to us!`;
 
-    let img = 'https://drive.google.com/file/d/1-ahJ4OdWp8l35NaABlwcII3Dx6rMzlFQ/view?usp=drivesdk'; 
+    let img = 'https://telegra.ph/file/4e8ee2ffed2ce1ba46309.jpg'; 
 
     console.log("Sending message and image...");
     await conn.sendFile(m.chat, img, 'donation.jpg', message, m, false, rpyp);
