12db90ce99cc8543a1838f832b05e24f9f43d25a
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2023-10-01 08:16:12 +0800

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By GURU`
+		let bio = `\nAnnisa Auwlia bot, Bot ini dibikin oleh [wa.me/6285156489458](Owner)🟢 Waktu Aktif ${muptime}\n\n`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
