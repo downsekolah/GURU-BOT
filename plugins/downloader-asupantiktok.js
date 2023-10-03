d628d885cd0a414ba20aa998a89044858e63a876
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2023-10-03 09:41:13 +0800

    Update downloader-asupantiktok.js

--- plugins/downloader-asupantiktok.js
+++ plugins/downloader-asupantiktok.js
@@ -41,6 +41,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['asupantiktok'].map(v => v + ' <username>');
 handler.tags = ['downloader'];
 handler.command = /^(asupantiktok)$/i;
-handler.limit = true;
+handler.limit = false;
 
 module.exports = handler;
