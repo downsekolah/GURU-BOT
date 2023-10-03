7448234844016e0cb0685870e9ac7332e9b22e97
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2023-10-03 13:47:57 +0800

    Update downloader-cecan.js

--- plugins/downloader-cecan.js
+++ plugins/downloader-cecan.js
@@ -16,4 +16,4 @@ let handler = async (m, { conn, command }) => {
 handler.command = ['china','vietnam','thailand','indonesia','korea','japan','malaysia','justinaxie','jeni','jiso','ryujin','rose','hijaber']
 handler.tags = ['downloader']
 
-module.exports = handler
+ export default handler 
