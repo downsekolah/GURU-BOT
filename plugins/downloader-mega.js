dc6c342e7edcb2ad75d0f3a90146e68f86302feb
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2023-10-03 16:19:33 +0800

    Update downloader-mega.js

--- plugins/downloader-mega.js
+++ plugins/downloader-mega.js
@@ -15,19 +15,8 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
         
         // Menambahkan ekstensi yang didukung (zip, rar, 7z, jpg, png) ke dalam daftar
         if (/mp4/.test(file.name)) {
-            await conn.sendMessage(m.chat, { document: data, mimetype: "video/mp4", filename: `${file.name}.mp4` }, { quoted: m });
-        } else if (/pdf/.test(file.name)) {
-            await conn.sendMessage(m.chat, { document: data, mimetype: "application/pdf", filename: `${file.name}.pdf` }, { quoted: m });
-        } else if (/zip/.test(file.name)) {
-            await conn.sendMessage(m.chat, { document: data, mimetype: "application/zip", filename: `${file.name}.zip` }, { quoted: m });
-        } else if (/rar/.test(file.name)) {
-            await conn.sendMessage(m.chat, { document: data, mimetype: "application/x-rar-compressed", filename: `${file.name}.rar` }, { quoted: m });
-        } else if (/7z/.test(file.name)) {
-            await conn.sendMessage(m.chat, { document: data, mimetype: "application/x-7z-compressed", filename: `${file.name}.7z` }, { quoted: m });
-        } else if (/jpg|jpeg/.test(file.name)) {
-            await conn.sendMessage(m.chat, { document: data, mimetype: "image/jpeg", filename: `${file.name}.jpg` }, { quoted: m });
-        } else if (/png/.test(file.name)) {
-            await conn.sendMessage(m.chat, { document: data, mimetype: "image/png", filename: `${file.name}.png` }, { quoted: m });
+             conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
+	m.react(done)
         } else {
             return m.reply('Error: Format file tidak didukung');
         }
@@ -36,8 +25,7 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     }
 }
 
-conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
-	m.react(done)
+
 
 handler.help = ["mega"]
 handler.tags = ["downloader"]
