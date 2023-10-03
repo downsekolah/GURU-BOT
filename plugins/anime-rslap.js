827603d4ab4fae5bbb925fed253b6c47a3181d46
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2023-10-03 20:59:44 +0800

    Update anime-rslap.js

--- plugins/anime-rslap.js
+++ plugins/anime-rslap.js
@@ -1,7 +1,16 @@
 import fetch from 'node-fetch'
-import { sticker } from '../lib/sticker.js'
-//import db from '../lib/database.js'
+import GIFBufferToVideoBuffer from '../lib/Gifbuffer.js';
 
+const getBuffer = async (url) => {
+  try {
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
+    return Buffer.from(buffer);
+  } catch (error) {
+    console.error("Failed to get buffer", error);
+    throw new Error("Failed to get buffer");
+  }
+}
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	
 	 let who
@@ -18,16 +27,22 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!rki.ok) throw await rki.text()
    let jkis = await rki.json()
    let { url } = jkis
-   let stiker = await sticker(null, url, `(${name2}) slapped`, `${name}`)
-   conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
-   m.react('👊🏻') 
+   const gifBuffer = await getBuffer(url)
+   const gifToVideoBuffer = await GIFBufferToVideoBuffer(gifBuffer);
+
+
+   conn.sendMessage(
+     m.chat,
+     { video: gifToVideoBuffer, caption: `(${name2}) slapped ${name}`, gifPlayback: true, gifAttribution: 0 },
+     { quoted: m }
+   );
+
    
 }
 
 handler.help = ['slap @tag']
 handler.tags = ['anime']
 handler.command = /^(slap|bofetada)$/i
-handler.diamond = true
-handler.group = true
+
 
 export default handler
